e4e738b6326c2f6ba1fc1b467883a858599dc5ad
NeilRow56 <neilrowland56@gmail.com>
2023-08-12 17:07:00 +0100

    Updated auth-modal for registration input

--- components/modals/auth-modal.tsx
+++ components/modals/auth-modal.tsx
@@ -1,18 +1,122 @@
 "use client"
 
+import * as z from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { Modal } from "@/components/ui/modal"
+import { Input } from "@/components/ui/input";
 import { useAuthModal } from "@/hooks/use-auth-modal"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+const FormSchema = z
+  .object({
+    name: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(8, 'Password must have than 8 characters'),
+    confirmPassword: z.string().min(1, 'Password confirmation is required'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Password do not match',
+  });
 
 export const AuthModal = () => {
     const authModal = useAuthModal()
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        },
+      });
+
+    const onSubmit = async (values: z.infer<typeof FormSchema> ) => {
+        console.log(values)
+        // TODO Register User
+    }
+
     return (
         <Modal
-   title="Register user"
-   description="Form description"
+   title="Register"
+   description="Register your company to start using the software"
    isOpen={authModal.isOpen}
    onClose={authModal.onClose}
    >
-    Future Register/Login Form
+    <div>
+    <div className="space-y-4 py-2 pb-4">
+    <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input   placeholder="Testing" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input  placeholder="testing@bt.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type='password'  placeholder="Enter your password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type='password'   placeholder="Confirm password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                  <Button variant="outline" onClick={authModal.onClose}>
+                    Cancel
+                  </Button>
+                  <Button  type="submit">Continue</Button>
+                </div>
+              </form>
+            </Form>
+</div>
+    </div>
+    
    </Modal>
     )
    
